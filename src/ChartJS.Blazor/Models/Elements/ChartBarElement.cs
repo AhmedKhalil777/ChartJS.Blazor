0cdf2f93060cedcb0553885825890676b1f2e3d8
Ahmed Khalil <ProgEng_Ahmed_khalil@outlook.com>
2021-05-29 23:56:17 +0200

    Add First commit

---
+++ src/ChartJS.Blazor/Models/Elements/ChartBarElement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartJS.Blazor
+{
+    public class ChartBarElement
+    {
+        public double BorderWidth { get; set; } = 1;
+    }
+}
